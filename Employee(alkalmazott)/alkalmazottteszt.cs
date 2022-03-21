3d30cd31b1d3649bffa3e53a2fbaa729e9f6fde0
Ákos Zarándi <79195687+Zakos100@users.noreply.github.com>
2022-03-21 17:07:32 +0100

    Add files via upload

---
+++ Employee(alkalmazott)/alkalmazottteszt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1_Employee
+{
+    class alkalmazottteszt
+    {
+        static void Main(string[] args)
+        {
+            alkalmazott employee = new alkalmazott();
+
+            employee.name = "Baumgartner Zsolt";
+            employee.salary = 150000;
+
+            Console.WriteLine(employee.DisplayInfo());
+            employee.IncreaseSalary(1000);
+            Console.WriteLine(employee.DisplayInfo());
+        }
+    }
+}
