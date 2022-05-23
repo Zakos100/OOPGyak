3dc066db462bb4c8c9c547eb32a97b44e67cb6a3
Ákos Zarándi <79195687+Zakos100@users.noreply.github.com>
2022-05-23 20:08:15 +0200

    Add files via upload

---
+++ 8.házi/Book/ITaxable.cs
@@ -0,0 +1,15 @@
+﻿namespace myinterface
+{
+    /*
+     Készítsen új névtérben (myinterface) adóztatható (ITaxable) interfészt.
+    Metódusai: getter, setter tulajdonságok az áfakulcshoz (TaxPercent), 
+    a double típusú adót kiszámító absztrakt metódus (GetTax()), 
+    és a double típusú adóval terhelt értéket kiszámító absztrakt metódus (GetTaxedValue())
+     */
+    interface ITaxable
+    {
+        int TaxPercent { get; set; }
+        double GetTaxedValue();
+        double GetTax();
+    }
+}
---
+++ 8.házi/README.md
@@ -0,0 +1,8 @@
+# beadando-8heti
+
+8. heti hazi feladat
+Feladat: szerkeszteni az ITaxable.cs, Product.cs., Book.cs, EBook.cs es Program.cs fajlokat
+
+Ellenorzes: Build/Build Solution (F6) es Test/Run All Tests
+
+es a Test Explorer-ben csak zold pipak legyenek (36 Passed)
